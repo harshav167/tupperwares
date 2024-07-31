79810bf4aa73fe4b1d4240441c04b9d2a1f1d0c6
harsha <mail@harsha.link>
2024-07-31 14:49:18 +1000

    add kali image

--- kali/build.sh
+++ kali/build.sh
@@ -1,3 +1,4 @@
 # docker buildx create --name mybuilder --use
 # docker buildx inspect --bootstrap
-docker buildx build --platform linux/arm64 -t kali-linux-arm --load . &
+# docker buildx build --platform linux/arm64 -t kali-linux-arm --load . &
+docker buildx build --platform linux/amd64,linux/arm64 --push -t ghcr.io/harshav167/tupperwares/kali:latest .
